6cabedd7dfcc3d0fd9dac946e11a9b4b2da81ba5
Andre-Claude Paulin <andre.claude.paulin@gmail.com>
2015-05-27 09:54:37 -0400

    Periodic update

---
+++ Parser/input3Gen.js
@@ -0,0 +1,97 @@
+var tmp;
+
+
+
+tmp = pool.addPlayer('Faucher');
+tmp.rounds[2].series.push(new Series('NY Rangers', '7', '8'));
+tmp.rounds[2].series.push(new Series('Chicago', '5', '7'));
+
+
+
+tmp = pool.addPlayer('Michou et Tony');
+tmp.rounds[2].series.push(new Series('Tampa Bay', '7', '7'));
+tmp.rounds[2].series.push(new Series('Anaheim', '7', '8'));
+
+
+
+tmp = pool.addPlayer('Doan');
+tmp.rounds[2].series.push(new Series('Tampa Bay', '6', '7'));
+tmp.rounds[2].series.push(new Series('Chicago', '6', '8'));
+
+
+
+tmp = pool.addPlayer('Michael Kemptner');
+tmp.rounds[2].series.push(new Series('NY Rangers', '7', '7'));
+tmp.rounds[2].series.push(new Series('Chicago', '7', '8'));
+
+
+
+tmp = pool.addPlayer('Philippe Brazeau');
+tmp.rounds[2].series.push(new Series('NY Rangers', '6', '8'));
+tmp.rounds[2].series.push(new Series('Anaheim', '6', '7'));
+
+
+
+tmp = pool.addPlayer('Andre Charette');
+tmp.rounds[2].series.push(new Series('NY Rangers', '6', '8'));
+tmp.rounds[2].series.push(new Series('Anaheim', '7', '7'));
+
+
+
+tmp = pool.addPlayer('Mohamed El Euch');
+tmp.rounds[2].series.push(new Series('NY Rangers', '7', '7'));
+tmp.rounds[2].series.push(new Series('Anaheim', '6', '8'));
+
+
+
+tmp = pool.addPlayer('Carl Lemay');
+tmp.rounds[2].series.push(new Series('Tampa Bay', '6', '7'));
+tmp.rounds[2].series.push(new Series('Chicago', '5', '8'));
+
+
+
+tmp = pool.addPlayer('Jee');
+tmp.rounds[2].series.push(new Series('Tampa Bay', '6', '7'));
+tmp.rounds[2].series.push(new Series('Chicago', '6', '8'));
+
+
+
+tmp = pool.addPlayer('Paul Cianfarano');
+tmp.rounds[2].series.push(new Series('Tampa Bay', '7', '7'));
+tmp.rounds[2].series.push(new Series('Chicago', '7', '8'));
+
+
+
+tmp = pool.addPlayer('Dany Lemieux');
+tmp.rounds[2].series.push(new Series('NY Rangers', '6', '7'));
+tmp.rounds[2].series.push(new Series('Chicago', '6', '8'));
+
+
+
+tmp = pool.addPlayer('Lowrie & Salhautmon');
+tmp.rounds[2].series.push(new Series('Tampa Bay', '6', '7'));
+tmp.rounds[2].series.push(new Series('Anaheim', '6', '8'));
+
+
+
+tmp = pool.addPlayer('Dede');
+tmp.rounds[2].series.push(new Series('NY Rangers', '5', '7'));
+tmp.rounds[2].series.push(new Series('Anaheim', '6', '8'));
+
+
+
+tmp = pool.addPlayer('ACP');
+tmp.rounds[2].series.push(new Series('NY Rangers', '5', '7'));
+tmp.rounds[2].series.push(new Series('Anaheim', '6', '8'));
+
+
+
+tmp = pool.addPlayer('Paolo Gianetti');
+tmp.rounds[2].series.push(new Series('NY Rangers', '6', '7'));
+tmp.rounds[2].series.push(new Series('Anaheim', '6', '8'));
+
+
+
+tmp = pool.addPlayer('Mike');
+tmp.rounds[2].series.push(new Series('NY Rangers', '6', '8'));
+tmp.rounds[2].series.push(new Series('Anaheim', '7', '7'));
